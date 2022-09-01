7e4c8b47bb93d97b9a45b061b7e920eb8cab99ad
socialuni <socialuni@163.com>
2022-09-01 13:32:08 +0800

    init

---
+++ src/const/TestA.ts
@@ -0,0 +1,7 @@
+import TestB from "@/const/TestB";
+
+export default class TestA {
+    static sasf() {
+        TestB.sasf()
+    }
+}
---
+++ src/const/TestB.ts
@@ -0,0 +1,5 @@
+export default class TestB {
+    static sasf(){
+        console.log(123)
+    }
+}
